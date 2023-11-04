c37cc204a9a655e3dc7e69d7cef60e7a003dc88e
Tom Schmitz <tom.schmitz16@gmail.com>
2023-11-04 15:41:28 +0100

    Added RSA Class

---
+++ SecureWire/Cryptography/RSA.cs
@@ -0,0 +1,52 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace SecureWire.Cryptography
+{
+    public class RSA
+    {
+        private RSAParameters _privateKey;
+        private RSAParameters _publicKey;
+
+        public RSA()
+        {
+            using (var rsa = new RSACryptoServiceProvider(2048))
+            {
+                _privateKey = rsa.ExportParameters(true);
+                _publicKey = rsa.ExportParameters(false);
+            }
+        }
+
+        public string Encrypt(string plainText, RSAParameters key)
+        {
+            using (var rsa = new RSACryptoServiceProvider())
+            {
+                rsa.ImportParameters(key);
+                byte[] plainBytes = Encoding.UTF8.GetBytes(plainText);
+                byte[] encryptedBytes = rsa.Encrypt(plainBytes, false);
+                return Convert.ToBase64String(encryptedBytes);
+            }
+        }
+
+        public string EncryptWithPublicKey(string plainText)
+        {
+            return Encrypt(plainText, _publicKey);
+        }
+
+        public string Decrypt(string cipherText, RSAParameters key)
+        {
+            using (var rsa = new RSACryptoServiceProvider())
+            {
+                rsa.ImportParameters(key);
+                byte[] cipherBytes = Convert.FromBase64String(cipherText);
+                byte[] decryptedBytes = rsa.Decrypt(cipherBytes, false);
+                return Encoding.UTF8.GetString(decryptedBytes);
+            }
+        }
+
+        public string DecryptWithPrivateKey(string cipherText)
+        {
+            return Decrypt(cipherText, _privateKey);
+        }
+    }
+}
